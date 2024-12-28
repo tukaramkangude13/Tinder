8b668d7518e8a1f78b9c1cac03a6036569896938
tukaramkangude13 <tukaramkangude13@gmail.com>
2024-12-28 13:29:41 +0530

    playing with multiple routing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,50 @@
-import express from 'express';
+import express from "express";
 const main = express();
 
+main.use("/login", (req, res) => {
+  res.send(" hello futh team  ");
+});
+main.use("/footer", (req, res) => {
+  res.send(" hello from the namste dev tinder backend  from the the  fotter ");
+});
+main.post(
+  "/user",
 
-main.use("/login",(req,res)=>{
-    res.send(" hello futh team  ")
-})
-main.use("/footer",(req,res)=>{
-    res.send(" hello from the namste dev tinder backend  from the the  fotter " )
-})
-main.use("/header",(req,res)=>{
-    res.send(" hello from the namste dev tinder backend  head of the project   ")
-})
-main.use("",(req,res)=>{
-    res.send(" hello from the namste     ")
-})
-main.use("/body",(req,res)=>{
-    res.send("hello")
-})
+  (req, res, next) => {
+    console.log(" fusrt response");
+    next();
+
+    // res.send("  first responce  1  ");
+  },
+  (req, res, next) => {
+    console.log(" responce number 2");
+    //   res.send("    responce number 2 ");
+    next();
+  },
+  (req, res, next) => {
+    console.log(" responce number 3");
+    next();
+
+    // res.send("    responce number 3 ");
+  }
+);
+
+main.use("/header", (req, res, next) => {
+  console.log(" responce number 1");
+  res.send("    responce number 1  ");
+});
+
+main.use("", (req, res, next) => {
+  console.log(" responce number 2 ");
+  res.send(" responce number 2     ");
+});
+
+main.use("/body", (req, res, next) => {
+  console.log("responce number 3");
+
+  res.send("responce number 3");
+});
 main.listen(7777, () => {
-  console.log('Server is running on port 7777');
+  console.log("Server is running on port 7777");
 });
 console.log(" server");
